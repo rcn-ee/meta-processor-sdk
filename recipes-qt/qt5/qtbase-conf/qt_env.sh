4d7422ad6fdd5eadde2e27c2bc818ce92240ba88
e-ruei1@ti.com <e-ruei1@ti.com>
2018-03-21 00:59:13 +0000

    qtbase-conf: set eglfs raw as the default QPA EGLFS mode
    Signed-off-by: Eric Ruei <e-ruei1@ti.com>
    Signed-off-by: Jacob Stiffler <j-stiffler@ti.com>

--- recipes-qt/qt5/qtbase-conf/qt_env.sh
+++ recipes-qt/qt5/qtbase-conf/qt_env.sh
@@ -3,3 +3,4 @@
 ### QT Environment Variables ###
 export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
 export QT_QPA_EGLFS_KMS_CONFIG=/etc/qt5/eglfs_kms_cfg.json
+export QT_QPA_EGLFS_INTEGRATION=none
