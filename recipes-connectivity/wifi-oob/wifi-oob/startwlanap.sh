f1a9d0226f68212a73abdbe4d3c445cdccf03148
Aparna M <a-m1@ti.com>
2022-01-14 22:35:27 +0530

    recipe-connectivity: wifi-oob: Modify script so the wifi demo fails gracefully on AM64x GP EVM
    Modify script so the wifi demo fails gracefully on AM64x GP EVM
    
    Signed-off-by: Aparna M <a-m1@ti.com>

--- recipes-connectivity/wifi-oob/wifi-oob/startwlanap.sh
+++ recipes-connectivity/wifi-oob/wifi-oob/startwlanap.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-defapname=AM64xSK-AP
+board_model=$(tr -d '\0' </proc/device-tree/model)
 
-cd /usr/share/wl18xx
+if [[ $board_model == *"AM642 SK"* ]]
+then
+    defapname=AM64xSK-AP
 
-./load_wlcore.sh
-sleep 1
+    cd /usr/share/wl18xx
 
-a3=$(iw wlan0 info | grep "addr" | awk '{split($0,a,":"); print a[3]}')
+    ./load_wlcore.sh
+    sleep 1
 
-a4=$(iw wlan0 info | grep "addr" | awk '{split($0,a,":"); print a[4]}')
+    a3=$(iw wlan0 info | grep "addr" | awk '{split($0,a,":"); print a[3]}')
 
-a5=$(iw wlan0 info | grep "addr" | awk '{split($0,a,":"); print a[5]}')
+    a4=$(iw wlan0 info | grep "addr" | awk '{split($0,a,":"); print a[4]}')
 
-apname="${defapname}_${a3}${a4}${a5}"
+    a5=$(iw wlan0 info | grep "addr" | awk '{split($0,a,":"); print a[5]}')
 
-sed -i "s/ssid=.*/ssid=${apname}/g" /usr/share/wl18xx/hostapd.conf
+    apname="${defapname}_${a3}${a4}${a5}"
 
-./ap_start.sh
-sleep 1
+    sed -i "s/ssid=.*/ssid=${apname}/g" /usr/share/wl18xx/hostapd.conf
 
-iperf3 -B 192.168.43.1 -p 6001 -s -i 5 &
+    ./ap_start.sh
+    sleep 1
+
+    iperf3 -B 192.168.43.1 -p 6001 -s -i 5 &
+fi
